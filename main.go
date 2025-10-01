53ab41c5e8cbdaa1d54fa027f08a558f4c1102bd
Majid Bhuiyan <65237569+majidbhuiyan20@users.noreply.github.com>
2025-10-01 23:39:46 +0600

    function with beautiful

--- main.go
+++ main.go
@@ -2,39 +2,59 @@ package main
 
 import "fmt"
 
-func add(num1 int, num2 int) int {
+func startLearning() {
+	fmt.Println("Hello Majid! Started Learning Go")
+}
 
+func add(num1 int, num2 int) int {
 	sum := num1 + num2
+	fmt.Println("Sum is = ", sum)
 	return sum
 }
 
 func getNumber(num1 int, num2 int) (int, int) {
 	sum := num1 + num2
 	mul := num1 * num2
-
+	fmt.Println("Sum is = ", sum)
+	fmt.Println("Mul is = ", mul)
 	return sum, mul
 }
 
 func printSomething() {
 	fmt.Println("Learn something with Habib vai")
 }
+
 func printWithString(name string) {
 	fmt.Println("Learn go Lang with ", name)
 }
 
+func nameInput() string {
+	var name string
+	fmt.Print("Enter Your Name - ")
+	fmt.Scan(&name)
+	fmt.Println("Your name is ", name)
+	return name
+}
+
+func getTwoNumber() (int, int, int) {
+	var num1 int
+	var num2 int
+	fmt.Print("Enter first number - ")
+	fmt.Scan(&num1)
+	fmt.Print("Enter Second number - ")
+	fmt.Scan(&num2)
+	sum := num1 + num2
+	fmt.Println("Sumation is - ", sum)
+	return num1, num2, sum
+}
+
 func main() {
-	fmt.Println("Hello Majid! Started Learning Go")
-
-	sum := add(10, 20)
-	fmt.Println(sum)
-
-	a := 20
-	b := 30
-
-	s, m := getNumber(a, b)
-	fmt.Println(s, m)
-
+	startLearning()
+	add(10, 20)
+	getNumber(12, 12)
 	printSomething()
 	printWithString("Habib vai")
+	nameInput()
+	getTwoNumber()
 
 }
