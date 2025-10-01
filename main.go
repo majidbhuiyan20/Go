f31d54991a9142d94bf42e1ee34779a4f2629dfd
Majid Bhuiyan <65237569+majidbhuiyan20@users.noreply.github.com>
2025-10-01 22:38:00 +0600

    basic add function

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func add(num1 int, num2 int) int {
+
+	sum := num1 + num2
+	return sum
+}
+
 func main() {
 	fmt.Println("Hello Majid! Started Learning Go")
+
+	sum := add(10, 20)
+	fmt.Println(sum)
 }
