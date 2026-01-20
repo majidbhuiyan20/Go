3d1e44fb1991cd1ab6b91fe2907b02205c4e022c
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-01-21 01:38:48 +0600

    practice go bankend

--- Test/main.go
+++ Test/main.go
@@ -8,48 +8,44 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Student model
 type Student struct {
-	Name string `json:"name"`
-	ID   string `json:"id"`
+	ID     string `json:"ID"`
+	Name   string `json:"Name"`
+	School string `json:"School"`
 }
 
-// In-memory student data
 var students = []Student{
-	{Name: "Majid Bhuiyan", ID: "101"},
+	{Name: "Majid Bhuiyan", ID: "101", School: "Jawar High School"},
 }
 
-// GET all students
 func getStudents(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "Application/json")
 	json.NewEncoder(w).Encode(students)
 }
 
-// POST new student
 func createStudent(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "Application/json")
 
 	var newStudent Student
+
 	err := json.NewDecoder(r.Body).Decode(&newStudent)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
-	}
 
+	}
 	students = append(students, newStudent)
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newStudent)
+
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	// Route GET request
 	r.HandleFunc("/students", getStudents).Methods("GET")
-
-	// Route POST request
 	r.HandleFunc("/students", createStudent).Methods("POST")
-
-	log.Println("Server running on http://localhost:8080")
+	log.Println("Server Running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
