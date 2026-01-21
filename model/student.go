05133f92ce5ec878edaaa574dd5fdda79954aa7d
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-01-22 00:53:55 +0600

    add student create and get request

---
+++ model/student.go
@@ -0,0 +1,7 @@
+package model
+
+type Student struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	School string `json:"school"`
+}
--- routes/routes.go
+++ routes/routes.go
@@ -2,11 +2,16 @@ package routes
 
 import (
 	"GO/handlers"
-
 	"github.com/gorilla/mux"
 )
 
+// RegisterProductRoutes registers product and student routes
 func RegisterProductRoutes(r *mux.Router) {
+	// Product routes
 	r.HandleFunc("/products", handlers.GetProducts).Methods("GET")
 	r.HandleFunc("/products", handlers.CreateProduct).Methods("POST")
+
+	// Student routes
+	r.HandleFunc("/students", handlers.GetStudents).Methods("GET")
+	r.HandleFunc("/students", handlers.CreateStudent).Methods("POST")
 }
